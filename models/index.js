b01da1d75774c65ce4841a51816b122bab947c40
davronkhamdamov <xamdamovdavron6@gmail.com>
2023-05-03 21:56:17 +0500

    models added index.js

--- models/index.js
+++ models/index.js
@@ -1,21 +1,27 @@
 import { makeExecutableSchema } from '@graphql-tools/schema'
 
 import TypeStaffs from './staffs/index.js'
-import TypePermisson_module from './permission_per/index.js'
 import TypeTransportModule from './transports/index.js'
 import TypeBranchModule from './branches/index.js'
+import TypePermisson_module from './permission_per/index.js'
+import TypeBranchPerModule from './brench_per/index.js'
+import TypeTransportPerModule from './transport_per/index.js'
 
 export default makeExecutableSchema({
     typeDefs: [
         TypeStaffs.typeDefs,
         TypePermisson_module.typeDefs,
         TypeTransportModule.typeDefs,
-        TypeBranchModule.typeDefs
+        TypeBranchModule.typeDefs,
+        TypeBranchPerModule.typeDefs,
+        TypeTransportModule.typeDefs,
     ],
     resolvers: [
         TypeStaffs.resolvers,
         TypePermisson_module.resolvers,
         TypeTransportModule.resolvers,
-        TypeBranchModule.resolvers
+        TypeBranchModule.resolvers,
+        TypeBranchPerModule.resolvers,
+        TypeTransportPerModule.resolvers,
     ]
 })
